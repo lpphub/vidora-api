f74dde88d330b3e4208b138b34ad974490a2ad1a
lsk <lsk@tt.com>
2026-03-18 16:35:31 +0800

    fix(tag): add soft delete to TagGroup model

--- modules/tag/model/group.go
+++ modules/tag/model/group.go
@@ -1,12 +1,17 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type TagGroup struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"size:50;not null" json:"name"`
-	SortOrder int       `gorm:"default:0" json:"sortOrder"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `gorm:"size:50;not null" json:"name"`
+	SortOrder int            `gorm:"default:0" json:"sortOrder"`
+	CreatedAt time.Time      `json:"createdAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func (*TagGroup) TableName() string {
